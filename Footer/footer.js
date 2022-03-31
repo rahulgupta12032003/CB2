0419c6693e8f3c21893c21918fdafdc1316798d7
Atanu Mondal <atanu.mondal.me48@gmail.com>
2022-03-31 10:56:04 +0530

    Footer Add
    Footer css & js added by atanu

---
+++ Footer/footer.js
@@ -0,0 +1,106 @@
+const footer = ()=>{
+    return `<div style="padding-top: 50px; background-color:white;margin:-10px">
+             <div style="display: flex; flex-direction: row; margin: 0px; padding-bottom: 30px;">
+                <span style="padding-left: 100px;font-family:'Courier New', Courier, monospace;font-size: 18px;font-weight: bold;">ON OUR RADAR</span>
+                <hr style="border-inline: 1px; width: 1000px;">
+             </div>
+             <div style="display: grid;grid-template-columns: repeat(4,22%);grid-template-rows:450px;gap:2%; justify-content: center;">
+                <a href="">
+                    <img src="https://cb2.scene7.com/is/image/CB2/121421_marquee_lighting_2?wid=680&qlt=75&resMode=sharp2" alt="" style="width: 100%; height:350px;">
+                    <p>BRUNA COLLECTION OF LIGHT</p>
+                </a>
+                <a href="../kristeenGiorgi.html">
+                    <img src="https://cb2.scene7.com/is/image/CB2/021822_home_bt_3?wid=740&qlt=50" alt="" style="width: 100%; height: 350px;">
+                    <p>ENTER THE LUSH, DREAMLIKE WORLD OF FINE ARTIST KRISTEN GIORGI</p>
+                </a>
+                <a href="">
+                    <img src="https://cb2.scene7.com/is/image/CB2/123021_super_furniture_livingRoom?wid=1000&qlt=50" alt="" style="width: 100%; height: 350px;">
+                    <p>LIVINGROOM AND OUTDOOR FURNITURE</p>
+                </a>
+                <a href="">
+                    <img src="https://cb2.scene7.com/is/image/CB2/123021_super_dining_drinkwareBar?wid=1000&qlt=50" alt="" style="width: 100%; height: 350px;">
+                    <p>KITCHEN STORAGE AND TOOLS</p>
+                </a>
+             </div>
+            </div>
+  <div id="footupA">
+    <!-- KNOW WHAT'S UP. SIGN UP FOR EMAILS. -->
+           <div>
+            <h4>KNOW WHAT'S UP. SIGN UP FOR EMAILS.</h4>
+            <input id="footInput" type="text" placeholder="Enter your email address">
+            <hr style="border: 1pxblack;">
+            <p style="font-size: 20px;">Read our:<a href="https://www.cb2.com/customer-service/privacy-policy#californiaNotice" class="underline_hover" style="font-size: 20px;">Privacy Policy</a></p>
+            <a href="https://www.facebook.com/cb2" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+            <a href="https://twitter.com/CB2" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+            <a href="https://www.pinterest.com/cb2/" target="_blank"><i class="fa fa-pinterest-p" aria-hidden="true"></i></a>
+            <a href="https://www.instagram.com/cb2/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+            <a href="https://www.youtube.com/channel/UCVqMrWK-KwKKC0zmavfJsuA" target="_blank"><i class="fa fa-youtube-play" aria-hidden="true"></i></a>
+           </div>
+    <!-- CONTACT US -->
+           <div>
+               <h5>CONTACT US</h5>
+               <p class="underline_hover"><a href="https://www.cb2.com/customer-service/">CUSTOMER SERVICE</a></p>
+               <p class="underline_hover"><a href="https://www.cb2.com/stores/">OUR STORES</a></p>
+               <p class="underline_hover"><a href="#">+1 (800) 606-6252</a></p>
+               <h5>CB2 CREDIT CARD</h5>
+               <p class="underline_hover"><a href="https://www.cb2.com/rewards/">APPLY NOW</a></p>
+               <p class="underline_hover"><a href="https://crateandbarrel.syf.com/login/">MANAGE ACCOUNT</a></p>
+           </div>
+    <!-- ORDER TRACKING & SCHEDULE DELIVERY -->
+           <div>
+               <h5>ORDER TRACKING &
+                   SCHEDULE DELIVERY</h5>
+               <p class="underline_hover"><a href="https://www.cb2.com/account/order-tracking">TRACK YOUR ORDER</a></p>
+               <p class="underline_hover"><a href="https://www.cb2.com/account/order-tracking">SCHEDULE DELIVERY</a></p>
+               <h5>OUR COMPANY</h5>
+               <p class="underline_hover"><a href="https://www.cb2.com/about-us/" style="padding-right:70px">ABOUT US</a></p>
+               <p class="underline_hover"><a href="https://jobs.cb2.com/?utm_source=cb2.com&utm_medium=referral&utm_campaign=Corporate_Site&_ga=2.155584059.1842362291.1648471483-445334869.1648471483" target="_blank">CAREERS</a></p>
+           </div>
+    <!-- RESOURCES -->
+           <div>
+               <h5>RESOURCES</h5>
+               <p class="underline_hover"><a href="#">MY ACCOUNT</a></p>
+               <p class="underline_hover"><a href="https://www.cb2.com/customer-service/faq-returns-exchanges">RETURN POLICY</a></p>
+               <p class="underline_hover"><a href="https://www.cb2.com/customer-service/faq-shipping-delivery">SHIPPING INFORMATION</a></p>
+               <p class="underline_hover"><a href="https://www.cb2.com/Customer-Service/email-preference">EMAIL PREFERENCES</a></p>
+               <p class="underline_hover"><a href="https://www.cb2.com/catalog/" style="padding-right:70px">CATALOGS</a></p>
+               <p class="underline_hover"><a href="https://www.cb2.com/gift-registry/" >GIFT REGISTRY</a></p>
+               <p class="underline_hover"><a href="https://www.cb2.com/gift-cards/" >GIFT CARDS</a></p>
+               <p class="underline_hover"><a href="https://www.cb2.com/cb2interiors">CB2 INTERIORS</a></p>
+               <p class="underline_hover"><a href="https://www.cb2.com/trade-program/">TRADE PROGRAM</a></p>
+               <p class="underline_hover"><a href="https://www.cb2.com/customer-service/product-recalls">PRODUCT RECALLS</a></p>
+               <p class="underline_hover"><a href="https://www.cb2.com/accessibility">ACCESSIBILITY STATEMENT</a></p>
+               <p class="underline_hover"><a href="https://www.cb2.com/ca-supply-chains-act">CA SUPPLY CHAINS ACT</a></p>
+           </div>
+    <!-- OUR BRANDS -->
+           <div>
+               <h5>OUR BRANDS</h5>
+               <h2><a href="https://www.crateandbarrel.com/?_ga=2.147113399.1842362291.1648471483-445334869.1648471483" target="_blank">Crate&Barrel</a></h2>
+               <h2><a href="https://www.crateandbarrel.com/kids/?_ga=2.147113399.1842362291.1648471483-445334869.1648471483" target="_blank">Crate&kids</a></h2>
+               <p><a href="https://hudsongracesf.com/?utm_source=cb2&_ga=2.211275157.1842362291.1648471483-445334869.1648471483" target="_blank" style="letter-spacing: .2rem;">HUDSON|GRACE</a></p>
+               <p><a href="#">SHIP TO: <img src="https://images.crateandbarrel.com/is/image/Crate/flag_US?wid=21" alt="" style="margin: 10px 0px 0px 10px;"></a></p>
+              
+           </div>
+       </div>
+
+    <!-- footer down section -->
+       <div id="footdownA">
+         <p>
+           <a href="https://privacyportal-cdn.onetrust.com/dsarwebform/ab7ff219-0933-4ed6-b071-e7c7eaf6147b/657a1ebe-2ff5-4cc0-a494-c76c46108908.html" target="_blank" class="underline_hover">DO NOT SELL MY PERSONAL INFORMATION </a>
+           <span>|</span>
+           <a href="" class="underline_hover">COOKIE SETTINGS</a>
+           <span>|</span>
+          <span>If you are using a screen reader and are having problems using this website, please call 800.606.6252 for assistance.</span>
+         </p>
+         <p>
+           <span>©2022 CB2</span>
+           <a href="https://www.cb2.com/customer-service/terms-of-use" class="underline_hover">TERMS OF USE</a>
+           <a href="https://www.cb2.com/customer-service/privacy-policy" class="underline_hover">PRIVACY POLICY</a>
+           <a href="https://www.cb2.com/site-index" class="underline_hover">SITE INDEX</a>
+           <a href="https://www.cb2.com/customer-service/privacy-policy" class="underline_hover">AD CHOICES</a>
+           <a href="" class="underline_hover">CO-BROWSE</a>
+        </p>
+       </div>`;
+}
+
+export default footer;
